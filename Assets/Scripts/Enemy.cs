59b640379749718700a3f270a4b5497c31fc809f
Agustin Galvan <agusstingalvan2@gmail.com>
2023-05-03 22:36:13 -0300

    se añadio enemigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 3.25f;
+    public float avoidDistance = 2f; // la distancia de detección de obstáculos
+    public float avoidForce = 5f; // la fuerza con la que el enemigo evita los obstáculos
 
-    private Vector3 _initialPositon;
-    // Start is called before the first frame update
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
+    private Vector3 _lastPosition;
+
     void Start()
     {
-        _initialPositon = transform.position;
+        _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
+        _lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        if (transform.position.z <= -10 || transform.position.x > 10)
+        // Encuentra la dirección actual del enemigo
+        Vector3 direction = (transform.position - _lastPosition).normalized;
+        _lastPosition = transform.position;
+
+        // Dispara un raycast en la dirección de movimiento actual para detectar obstáculos
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, direction, out hit, avoidDistance, LayerMask.GetMask("Obstacle")))
         {
-            transform.position = new Vector3(_initialPositon.x, transform.position.y, _initialPositon.z);
+            // Calcula un nuevo vector de dirección girando hacia la derecha y alejándose del obstáculo
+            Vector3 avoidDirection = Vector3.Cross(direction, Vector3.forward).normalized;
+            Vector3 newDirection = Vector3.Cross(avoidDirection, direction).normalized;
+
+            // Asigna la nueva dirección al enemigo
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
+        else
+        {
+            // Si no se detecta un obstáculo, continúa moviéndote hacia adelante
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        // Si el enemigo se sale del mapa, vuelve a la posición inicial
+        if (transform.position.z <= -10 || transform.position.x > 10 || transform.position.z > 108f)
+        {
+            transform.position = new Vector3(_initialPosition.x, transform.position.y, _initialPosition.z);
+            transform.rotation = _initialRotation;
         }
     }
 }
